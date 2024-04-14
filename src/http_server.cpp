91c6fd7cfe25b8a049994528992378e00b758aab
sebastienldegroof <sebastien.l.degroof@gmail.com>
2024-04-14 16:32:16 -0500

    stage 5

--- src/http_server.cpp
+++ src/http_server.cpp
@@ -1,7 +1,5 @@
 #include "http_server.h"
 
-#include <string>
-
 std::string get_path (char* recv_buffer) {
   int pre_space {3};
   while (recv_buffer[pre_space-3]!='G' || recv_buffer[pre_space-2]!='E' || recv_buffer[pre_space-1]!='T') {
@@ -21,14 +19,57 @@ std::string get_path (char* recv_buffer) {
   return req_path;
 }
 
-std::string process_request (char* recv_buffer) {
-    std::string path = get_path(recv_buffer);
+std::map<std::string, std::string> get_headers (char* recv_buffer) {
+  std::map<std::string, std::string> headers;
+
+  int count {1};
+  // increase count until the end of the first line \r\n
+  while (recv_buffer[count-1]!='\n') count += 1;
+  
+  std::string key {};
+  std::string value {};
+  // continue looping until the double return before the body
+  while (recv_buffer[count-2]!='\r' || 
+         recv_buffer[count-1]!='\n' || 
+         recv_buffer[count]!='\r' || 
+         recv_buffer[count+1]!='\n') {
+    key = "";
+    value = "";
+
+    // find the colon and store the chars in the key string
+    while (recv_buffer[count]!=':') {
+      key += recv_buffer[count];
+      count += 1;
+    }
+    count += 2; // skip past the ": "
+
+    // find the end of the line and store the chars in the value string
+    while (recv_buffer[count]!='\r') {
+      value += recv_buffer[count];
+      count += 1;
+    }
+    count += 2; // skip past the \r\n
     
-    if (path == "/") return "HTTP/1.1 200 OK\r\n\r\n";
+    headers[key] = value;
+  }
+
+  return headers;
+}
+
+std::string process_request (char* recv_buffer) {
+  std::string path = get_path(recv_buffer);
+    
+  if (path == "/") return "HTTP/1.1 200 OK\r\n\r\n";
     else if (path.substr(0,6) == "/echo/") {
-        std::string echo {path.substr(6)};
-        return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
-        + std::to_string(echo.length()) + "\r\n\r\n" + echo;
+      std::string echo {path.substr(6)};
+      return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
+      + std::to_string(echo.length()) + "\r\n\r\n" + echo;
+    }
+    else if (path == "/user-agent") {
+      std::map<std::string, std::string> headers = get_headers(recv_buffer);
+      std:: string user_agent {headers["User-Agent"]};
+      return "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " 
+      + std::to_string(user_agent.length()) + "\r\n\r\n" + user_agent;
     }
     else return "HTTP/1.1 404 Not Found\r\n\r\n";
 }
--- src/http_server.h
+++ src/http_server.h
@@ -1,6 +1,9 @@
 #include <string>
 #include <iostream>
+#include <map>
 
 std::string get_path (char* recv_buffer);
+
+std::map<std::string, std::string> get_headers (char* recv_buffer);
 
 std::string process_request (char* recv_buffer);
